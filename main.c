e858067af7a0f8f66f600ef65e562472641a5428
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-05-27 13:25:35 -0300

    solve one queens function on main

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "eightqueens.h"
+#include <stdio.h>
 
 int main() {
     int board[SIZE];
+    startboard(board);
+
+    if (!solveonequeens(board, 0)) printf("No solution was found!\n");
+
     startboard(board);
 
     solvequeens(board, 0);
